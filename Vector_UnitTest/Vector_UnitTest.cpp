a5edd94064daca9815d2851d25c489147d0b249d
hadnett <williamsrhadnett@outlook.com>
2021-01-21 00:16:43 +0000

    Add unit test for add method

--- Vector_UnitTest/Vector_UnitTest.cpp
+++ Vector_UnitTest/Vector_UnitTest.cpp
@@ -22,3 +22,12 @@ TEST_F(Vector_UnitTest, testConstructor) {
     Vector vector(5);
     ASSERT_EQ(vector.capacity, 5);
 }
+
+TEST_F(Vector_UnitTest, testAdd) {
+    Vector vector(5);
+
+    vector.add(1);
+    vector.add(2);
+
+    ASSERT_EQ(2, vector.count);
+}
